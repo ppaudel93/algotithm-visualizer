56ae5409a18f4a1c92128d49507ba6975e87f648
Prayog <ppaudel93@gmail.com>
2019-10-22 19:10:51 +0545

    Multiple components, setup

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import './static/variables.sass'
+import './static/main.sass'
 
 Vue.config.productionTip = false
+Vue.use(Buefy)
 
 new Vue({
+  el: '#app',
   router,
   store,
+  components: { App },
   render: h => h(App)
 }).$mount('#app')
