ff82f7262365281b3e528ca124b65cde578a62b8
Dipesh Paudel <dipesh.paudel@cclsolution.com>
2020-02-06 20:46:32 +0545

    dip-wind-additon

---
+++ src/components/_base-component-loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-loader"/>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass">
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import BaseHeader from './_base-header'
 import BaseParallax from './_base-parallax'
 import BaseImage from './_base-image'
 import BaseIcon from './_base-icon'
+import BaseComponentLoader from './_base-component-loader'
 
 export {
   BaseButton,
@@ -10,4 +11,5 @@ export {
   BaseParallax,
   BaseImage,
   BaseIcon,
+  BaseComponentLoader,
 }
